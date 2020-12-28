60f4821937f57d10171bd34153944848ccbd0eb3
Anand Rajagopal <anand.rajagopal@icloud.com>
2020-12-28 14:13:43 -0600

    A much more simplified and predictable successor implementation

--- bst/successor/main.go
+++ bst/successor/main.go
@@ -47,43 +47,23 @@ func printAscendingOrder(node *Node) {
 
 }
 
-func findSuccessor(node *Node, value int) int {
-	if node == nil {
-		return -1
-	}
-	var parent *Node
-	if value < node.value {
-		parent = locateLeft(node, value)
-	} else {
-		parent = locate(node, value)
-	}
-	successor := successor(parent, value)
-	if value == successor {
-		return parent.value
-	}
-	return successor
+func succ(node *Node, value int) int {
+	return successor(node, value, node.value)
 }
 
-func locateLeft(node *Node, value int) *Node {
-	if node.left == nil || value >= node.left.value {
-		return node
+func successor(node *Node, value, potentialSuccessor int) int {
+	if value == node.value && node.right == nil {
+		if potentialSuccessor < node.value {
+			return -1
+		}
+		return potentialSuccessor
 	}
-	return locateLeft(node.left, value)
-}
-
-func locate(node *Node, value int) *Node {
-	if node.right == nil || value < node.value {
-		return node
-	}
-	// if node
-	return locate(node.right, value)
-}
-
-func successor(node *Node, value int) int {
 	if value < node.value && node.left != nil {
-		return successor(node.left, value)
-	} else if value >= node.value && node.right != nil {
-		return successor(node.right, value)
+		potentialSuccessor = node.value
+		return successor(node.left, value, potentialSuccessor)
+	}
+	if value >= node.value && node.right != nil {
+		return successor(node.right, value, potentialSuccessor)
 	}
 	return node.value
 }
@@ -110,19 +90,18 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("finding successor")
-	fmt.Println("0 - ", findSuccessor(root, 0))
-	fmt.Println("1 - ", findSuccessor(root, 1))
-	fmt.Println("2 - ", findSuccessor(root, 2))
-	fmt.Println("3 - ", findSuccessor(root, 3))
-	fmt.Println("4 - ", findSuccessor(root, 4))
-	fmt.Println("5 - ", findSuccessor(root, 5))
-	fmt.Println("6 - ", findSuccessor(root, 6))
-	fmt.Println("7 - ", findSuccessor(root, 7))
-	fmt.Println("8 - ", findSuccessor(root, 8))
-	fmt.Println("9 - ", findSuccessor(root, 9))
-	fmt.Println("12 - ", findSuccessor(root, 12))
-	fmt.Println("13 - ", findSuccessor(root, 13))
-	fmt.Println("14 - ", findSuccessor(root, 14))
-
+	fmt.Println("finding successor - new way")
+	fmt.Println("0 - ", succ(root, 0))
+	fmt.Println("1 - ", succ(root, 1))
+	fmt.Println("2 - ", succ(root, 2))
+	fmt.Println("3 - ", succ(root, 3))
+	fmt.Println("4 - ", succ(root, 4))
+	fmt.Println("5 - ", succ(root, 5))
+	fmt.Println("6 - ", succ(root, 6))
+	fmt.Println("7 - ", succ(root, 7))
+	fmt.Println("8 - ", succ(root, 8))
+	fmt.Println("9 - ", succ(root, 9))
+	fmt.Println("12 - ", succ(root, 12))
+	fmt.Println("13 - ", succ(root, 13))
+	fmt.Println("14 - ", succ(root, 14))
 }
