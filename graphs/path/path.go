46ac46d29e29d424152e8add14f16dfda400e365
Anand Rajagopal <anand.rajagopal@icloud.com>
2020-12-31 14:16:03 -0600

    adjusting visibility

--- graphs/path/path.go
+++ graphs/path/path.go
@@ -8,8 +8,8 @@ type Path struct {
 // NewPath makes a new path
 func NewPath() *Path {
 	p := &Path{
-		visited: make(map[int]int),
-		paths:   make(map[int]bool),
+		Visited: make(map[int]int),
+		Paths:   make(map[int]bool),
 	}
 
 	return p
@@ -17,19 +17,19 @@ func NewPath() *Path {
 
 // Visit the given vertex
 func (p *Path) Visit(vertex, from int) {
-	p.visited[vertex] = from
-	p.paths[from] = true
+	p.Visited[vertex] = from
+	p.Paths[from] = true
 }
 
 // Visited returns if the vertex has been visited already
 func (p *Path) Visited(vertex int) (bool, int) {
-	from, ok := p.visited[vertex]
+	from, ok := p.Visited[vertex]
 	return ok, from
 }
 
 // CycleDetected returns true if the vertex is the current path
 func (p *Path) CycleDetected(vertex int) bool {
-	_, ok := p.paths[vertex]
+	_, ok := p.Paths[vertex]
 	if ok {
 		return true
 	}
